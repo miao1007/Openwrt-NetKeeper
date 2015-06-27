56a5c769a8261802fdaa7df1c28245124ee58f7d
leon <miao1007@gmail.com>
2015-06-27 13:12:08 +0800

    Add supoort to Nanchang
    Update readme

--- src/sxplugin.c
+++ src/sxplugin.c
@@ -1,19 +1,3 @@
-/***************************************************************************
-* Copyright 2013,2014 miao1007@gmail.com
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*   http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-* *************************************************************************
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -51,7 +35,8 @@ static void getPIN(byte *userName, byte *PIN)
     * ChongQing Netkeeper(0094): xianxinli1radius
     * WuHan E xin: hubtxinli01
     * Hangzhou(Tested on HDU): singlenet01
-    * NanChang: nanchang3.0
+    * NanChangV18: nanchang3.0
+    * NanChangV12~V17: radius
     **/
     strcpy(RADIUS, "cqxinliradius002");
     timenow = time(NULL);
