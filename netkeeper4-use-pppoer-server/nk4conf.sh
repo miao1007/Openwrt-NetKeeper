613a4696b62457676af590b501133718fe82b647
huipeng <huipengly@gmail.com>
2017-06-09 21:58:02 +0800

    修改帐号密码截获方法

--- netkeeper4-use-pppoer-server/nk4conf.sh
+++ netkeeper4-use-pppoer-server/nk4conf.sh
@@ -1,25 +1,18 @@
 #!/bin/sh
-#测试版本4.7.9.589，@cqupt
 
-#安装pppoe-server
-#opkg update
-#opkg install rp-pppoe-server
+#Check PPPoE-Server
+if [ -z "$(opkg list-installed | grep "rp-pppoe-server")" ]
+then
+    echo "Please install PPPoE-Server first"
+    exit 0
+fi
 
-#/etc/ppp/option 改 logfile /dev/null 为 logfile /tmp/pppoe.log
-sed -i "s/dev/tmp/" /etc/ppp/options
-sed -i "s/null/pppoe.log/" /etc/ppp/options
-#pppoe-server带的rp-pppoe.so无法加载，调用openwrt自带的rp-pppoe.so
-cp /etc/ppp/plugins/rp-pppoe.so /etc/ppp/plugins/rp-pppoe.so.bak
-cp /usr/lib/pppd/2.4.7/rp-pppoe.so /etc/ppp/plugins/rp-pppoe.so
-#/etc/ppp/pppoe-server-options 改require-pap为require-chap
-#sed -i "s/require-pap/require-chap/" /etc/ppp/pppoe-server-options
-#/etc/ppp/pap-secrets添加一个用户(不添加无法运行)
-cp /etc/ppp/pap-secrets /etc/ppp/pap-secrets.bak
-sed -i '$d' /etc/ppp/pap-secrets
-echo "test * test *">>/etc/ppp/pap-secrets
+#change log location & enable debug & show password
+sed -i "s/\/dev\/null/\/tmp\/pppoe.log/" /etc/ppp/options
+sed -i "s/#debug/debug/" /etc/ppp/options
+echo "show-password" >> /etc/ppp/options
 
-#参考confnetwork.sh。修改ifname根据wan口设置
-#uci delete network.wan
+#set network
 uci delete network.wan6
 uci commit network
 
@@ -27,11 +20,11 @@ uci set network.netkeeper=interface
 uci set network.netkeeper.ifname=$(uci show network.wan.ifname | awk -F "'" '{print $2}')
 uci set network.netkeeper.macaddr=aabbccddeeff
 uci set network.netkeeper.proto=pppoe
-#TODO:set pppoe password
 uci set network.netkeeper.username=username
 uci set network.netkeeper.password=password
 uci set network.netkeeper.metric='0'
 uci commit network
+
 #set firewall
 uci set firewall.@zone[1].network='wan netkeeper' 
 uci commit firewall
@@ -39,13 +32,13 @@ uci commit firewall
 /etc/init.d/network reload
 /etc/init.d/network restart
 
-#使pppoe支持转义字符
+#enable \r in PPPoE
 cp /lib/netifd/proto/ppp.sh /lib/netifd/proto/ppp.sh_bak
 sed -i '/proto_run_command/i username=`echo -e "$username"`' /lib/netifd/proto/ppp.sh
-#sed -i '/proto_run_command/i password=`echo -e "$password"`' /lib/netifd/proto/ppp.sh
 
-#设置启动脚本
+#set init script
 cp /root/nk4 /etc/init.d/nk4
 chmod +x /etc/init.d/nk4
 /etc/init.d/nk4 enable
-/etc/init.d/nk4 start &
+sleep 5
+(/etc/init.d/nk4 start &)
